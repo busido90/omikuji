029e0f69770db8f345cffb7e4ffc5bc9790491ea
busido90 <nagatai0120@gmail.com>
2015-04-10 11:51:27 +0800

    omikuji

--- sampleOmikuji/ViewController.h
+++ sampleOmikuji/ViewController.h
@@ -10,6 +10,10 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+    
+- (IBAction)alertButton:(id)sender;
+
 
 @end
 
--- sampleOmikuji/ViewController.m
+++ sampleOmikuji/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSArray *_omikujilist;
+    NSArray *_dolist;
+    int _rNum;
+}
+
 
 @end
 
@@ -17,6 +23,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +32,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)alertButton:(id)sender {
+    
+    _omikujilist = [NSArray arrayWithObjects:@"大吉", @"中吉", @"小吉", @"吉", @"凶", nil];
+    
+    _dolist = [NSArray arrayWithObjects:@"サッカー", @"テニス", @"野球", @"ソフトボール", @"ラグビー", nil];
+    
+    _rNum = arc4random() % 5;
+    
+    UIAlertView     *alert = [[UIAlertView alloc] initWithTitle:@"今日の運勢？" message:_omikujilist[_rNum] delegate: self cancelButtonTitle:@"もう一度" otherButtonTitles:@"ラッキーアイテム",nil];
+    
+    [alert show];
+}
+
+- (void)alertView: (UIAlertView *)alertView clickedButtonAtIndex:
+(NSInteger)buttonIndex {
+    NSLog(@"%ld", (long)buttonIndex);
+    
+    if (buttonIndex == 1) {
+    self.resultLabel.text = _dolist[_rNum];
+    }
+}
 @end
